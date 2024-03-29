7024d1b5752e71762c14764bde79ea520409b370
Firman <165023206+FirmanEVGR@users.noreply.github.com>
2024-03-30 05:11:54 +0700

    update plugins/_autotiktok.js

--- plugins/_autotiktok.js
+++ plugins/_autotiktok.js
@@ -5,16 +5,14 @@ handler.before = async function (m, { text, conn }) {
 const isCmd =/^[°•π÷×¶∆£¢€¥®™+✓_=|~!?@#$%^&.©^]/gi.test(m.text)
 let urlTiktokRegex = /(https?:\/\/[^\s]*tiktok\.com[^\s]*)/g;
 var budy = (typeof m.text == 'string' ? m.text : '')
-let chat = db.data.chats[m.chat]
-if(m.isGroup && !chat.autotiktok) return
 if(!isCmd && budy.match(urlTiktokRegex)){
 let res = budy.match(urlTiktokRegex)[0]
 m.reply(mess.wait)
 let anu = await tiktok(res)
 let capt = `*Username :* ${anu.author}
 *Deskripsi :* ${anu.desc}`
-let qu = await conn.sendFile(m.chat, anu.video, "", capt, m)
-await conn.sendMessage(m.chat, {
+let qu = await conn.sendFile(m.sender, anu.video, "", capt, m)
+await conn.sendMessage(m.sender, {
 audio: { url: anu.audio },
 mimetype: "audio/mpeg"
 },{ quoted: qu })
