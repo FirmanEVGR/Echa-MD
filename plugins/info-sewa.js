598361dc6031887e3c8b85b74db0eb2e4d2b6e79
Firman <165023206+FirmanEVGR@users.noreply.github.com>
2024-03-30 07:30:36 +0700

    update plugins/info-sewa.js

--- plugins/info-sewa.js
+++ plugins/info-sewa.js
@@ -1,10 +1,49 @@
-let fetch = require('node-fetch')
+let handler = async (m, { conn }) => {
+let img = 'https://telegra.ph/file/452b1f5af8b657a430a1f.jpg'
+let capt = `*— SEWA BOT*
 
-let handler = async (m, { conn, command }) => {
-    let buffer = await fetch(`https://telegra.ph/file/1c53a85e78e81e2aaad9c.jpg`).then(res => res.buffer())
-    conn.sendFile(m.chat, buffer, 'hasil.jpg', `*Jika telah melakukan pembayaran silahkan kirimkan bukti pembayaran ke WhatsApp Owner.*`, m)
+*Sewa Bot 7 Hari*
+- Harga: Rp 3.000
+
+*Sewa Bot 14 Hari*
+- Harga: Rp 6.000
+
+*Sewa Bot 30 Hari*
+- Harga: Rp 10.000
+
+*Sewa Bot 1 Tahun*
+- Harga: Rp 119.000
+
+
+*— UPDATE PREMIUM*
+
+*Premium 7 Hari*
+- Harga: Rp 2.000
+
+*Premium 15 Hari*
+- Harga: Rp 3.000
+
+*Premium 30 Hari*
+- Harga: Rp 5.000
+
+*Premium 1 Tahun*
+- Harga: Rp 99.000`
+conn.sendMessage(m.chat, {
+text: capt,
+contextInfo: { 
+mentionedJid: [m.sender],
+externalAdReply: { 
+title: '',
+body: '',
+thumbnailUrl: img,
+sourceUrl: "https://wa.me/6283861518933?text=Assalamualaikum+Kak",
+mediaType: 1,
+renderLargerThumbnail: true
+}}
+})
 }
-
-handler.help = handler.command = ['donasi','donate','sewa','sewabot','belibot']
+handler.help = ['sewa', 'sewabot', 'buyprem', 'premium']
 handler.tags = ['main']
+handler.command = /^(buyprem|sewa|sewabot|prem|premium)$/i
+
 module.exports = handler
